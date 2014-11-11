ef0eb3c94a2872f044df73f9eacec96778f6ca8c
myun2 <myun2@nwhite.info>
2014-11-11 19:42:48 +0900

    init rails-chart

---
+++ lib/rails/chart.rb
@@ -0,0 +1,7 @@
+require "rails/chart/version"
+
+module Rails
+  module Chart
+    # Your code goes here...
+  end
+end
---
+++ lib/rails/chart/version.rb
@@ -0,0 +1,5 @@
+module Rails
+  module Chart
+    VERSION = "0.0.1"
+  end
+end
